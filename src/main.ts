4ac7380d2ccbf79ba4207fba566ccec6b68b0474
Luke Taylor <luke@circleleasing.co.uk>
2025-02-14 10:51:31 +0000

    Overall graphs

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,54 @@ import { engine } from 'express-handlebars';
 import { readFileSync } from 'fs';
 import path from 'path';
 
+type StatType = "Shot" | "Cross" | "Corner";
+
+type Event = {
+    id: number;
+    statTypeID: number;
+    statType: StatType;
+    time: number;
+    outcomeId: number;
+    outcome: string;
+}
+
+type Segment = {
+    name: string;
+    code: string;
+    startTime: number;
+    duration: 45 | 15;
+    events: {
+        home: Event[];
+        away: Event[];
+    }
+}
+
+type Data = {
+    homeTeam: string;
+    awayTeam: string;
+    homeScore: number;
+    awayScore: number;
+    segments: Segment[];
+};
+
 const app = express();
 
 app.engine("hbs", engine({
     extname: ".hbs",
     helpers: {
         json: (obj: any) => JSON.stringify(obj),
-        eachStat: (segment: any, options: Handlebars.HelperOptions) => {
-            const combined = [...segment.events.home.map((e: any) => ({
+        eachStat: (segment: Segment, options: Handlebars.HelperOptions) => {
+            const combined = [...segment.events.home.map(e => ({
                 ...e,
                 isHome: true
-            })), ...segment.events.away.map((e: any) => ({
+            })), ...segment.events.away.map(e => ({
                 ...e,
                 isHome: false
             }))].sort((a, b) => a.time - b.time);
 
             return combined.map(event => options.fn(event)).join("");
         },
-        eachStatCategory: (segment: any, options: Handlebars.HelperOptions) => {
+        eachStatCategory: (segment: Segment, options: Handlebars.HelperOptions) => {
             const categories: { [key: string]: any } = {
                 "Cross": {
                     home: 0,
@@ -69,7 +99,7 @@ app.engine("hbs", engine({
                 }
             }
 
-            segment.events.home.forEach((event: any) => {
+            segment.events.home.forEach(event => {
                 categories[event.statType].home++;
                 Object.keys(substatConfig[event.statType]).forEach(k => {
                     if(substatConfig[event.statType][k].some(x => x == event.outcomeId)){
@@ -78,7 +108,7 @@ app.engine("hbs", engine({
                 })
             });
 
-            segment.events.home.forEach((event: any) => {
+            segment.events.home.forEach(event => {
                 categories[event.statType].away++;
                 Object.keys(substatConfig[event.statType]).forEach(k => {
                     if(substatConfig[event.statType][k].some(x => x == event.outcomeId)){
@@ -113,9 +143,11 @@ app.engine("hbs", engine({
             const seconds = Math.floor(time % 60);
             return minutes.toString().padStart(2, '0') + ":" + seconds.toString().padStart(2, '0');
         },
-        getRoutes: () => ["Timeline", "Stats"],
+        getRoutes: () => ["Timeline", "Stats", "Graphs"],
         eq: (a: any, b: any) => a == b,
-        lower: (str: string) => str.toLowerCase()
+        lower: (str: string) => str.toLowerCase(),
+        multiply: (a: number, b: number) => a * b,
+        add: (a: number, b: number) => a + b
     }
 }));
 
@@ -124,7 +156,7 @@ app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, "../public")));
 
 const getData = () => {
-    const data = JSON.parse(readFileSync(path.join(__dirname, "data.json"), "utf8"));
+    const data: Data = JSON.parse(readFileSync(path.join(__dirname, "data.json"), "utf8"));
     const title = `${data.homeTeam} ${data.homeScore}-${data.awayScore} ${data.awayTeam}`;
     return {data, title};
 }
@@ -135,14 +167,85 @@ app.get('/stats', (_, res) => {
     const data = getData();
 
     //add a dummy "Overall" segment
-    const home = data.data.segments.flatMap((segment: any) => segment.events.home);
-    const away = data.data.segments.flatMap((segment: any) => segment.events.home);
+    const home = data.data.segments.flatMap(segment => segment.events.home);
+    const away = data.data.segments.flatMap(segment => segment.events.home);
     data.data.segments.unshift({
         name: "Overall",
-        events: {home, away}
+        startTime: 0,
+        events: {home, away},
+        duration: 45,
+        code: ""
     });
 
     res.render('stats.hbs', data)
+});
+app.get('/graphs', (_, res) => {
+    const {data, title} = getData();
+
+    const stats: {
+        momentum: number[][],
+        home: Record<StatType, number>[][],
+        away: Record<StatType, number>[][],
+        segments: {name: string, code: string}[],
+        maxMomentum: number,
+        maxStats: number
+    } = {
+        momentum: [],
+        home: [],
+        away: [],
+        segments: [],
+        maxMomentum: 0,
+        maxStats: 0
+    };
+
+    data.segments.forEach((segment, ix) => {
+        const numSegments = Math.floor(segment.duration / 5);
+        const momentum: number[] = Array(numSegments).fill(0);
+        //if we just fill these with [], JS makes it a reference to the same object (i.e. [].push pushes to ALL)
+        const home: Record<StatType, number>[] = Array(numSegments).fill(0).map(_ => ({Shot: 0, Cross: 0, Corner: 0}));
+        const away: Record<StatType, number>[] = Array(numSegments).fill(0).map(_ => ({Shot: 0, Cross: 0, Corner: 0}));
+
+        segment.events.home.forEach(e => {
+            let section = Math.floor((((e.time - segment.startTime) / 1000) / 60) / 5);
+            if(section >= numSegments){
+                section = numSegments - 1; //added time
+            }
+            
+            momentum[section]++;
+            home[section][e.statType]++;
+        });
+
+        segment.events.away.forEach(e => {
+            let section = Math.floor((((e.time - segment.startTime) / 1000) / 60) / 5);
+            if(section >= momentum.length){
+                section = momentum.length; //added time
+            }
+
+            momentum[section]--;
+            away[section][e.statType]++;
+        });
+
+        stats.momentum.push(momentum);
+        stats.home.push(home);
+        stats.away.push(away);
+        stats.segments.push({name: segment.name, code: segment.code});
+
+        for(let i = 0; i < numSegments; i++){
+            if(momentum[i] > stats.maxMomentum){
+                stats.maxMomentum = momentum[i];
+            }
+
+            if(home[i].Shot + home[i].Cross + home[i].Corner > stats.maxStats){
+                stats.maxStats = home[i].Shot + home[i].Cross + home[i].Corner;
+            }
+
+            if(away[i].Shot + away[i].Cross + away[i].Corner > stats.maxStats){
+                stats.maxStats = away[i].Shot + away[i].Cross + away[i].Corner;
+            }
+        }
+    });
+
+    res.render('graphs.hbs', {title, ...stats});
 });
 
 app.listen(3000, () => "Listening on port 3000!");
